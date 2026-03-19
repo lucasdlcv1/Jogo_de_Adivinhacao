4c383e0c12115487fb67b996df8ac4ebacd8ad58
Lucas de Lima <lucasdlcvieira@gmail.com>
2026-03-19 20:07:27 -0300

    Adicionada geracao de numero aleatorio com cryptography

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+/*
+
+*/
+
+// 1. O Jogo deve aceitar o input do jogador
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine("Jogo de adivinhacao");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+Console.Write("Digite um numero: ");
+string strNumeroDigitado = Console.ReadLine();
+
+//gera numero de 1 a 20 (numero minimo, numero maximo(exclusivo))
+int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+
+
+Console.WriteLine("O numero aleatorio3 foi: " + numeroAleatorio);
+
+Console.ReadLine();
