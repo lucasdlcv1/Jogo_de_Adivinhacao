a80c3f425d2da4d34bd2f68d40e953120c2f821a
Lucas de Lima <lucasdlcvieira@gmail.com>
2026-03-19 20:31:50 -0300

    Adicionada a comparacao entre os numeros

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,8 @@
 */
 
 // 1. O Jogo deve aceitar o input do jogador
+// 2. Gerar um numero aleatorio
+// 3. o jogo dedve validaar a tentativa e dar feedback
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine("Jogo de adivinhacao");
 Console.WriteLine("--------------------------------------------");
@@ -12,10 +14,27 @@ Console.WriteLine();
 Console.Write("Digite um numero: ");
 string strNumeroDigitado = Console.ReadLine();
 
+int numeroDigitado = Convert.ToInt32(strNumeroDigitado);
+
 //gera numero de 1 a 20 (numero minimo, numero maximo(exclusivo))
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
 
-Console.WriteLine("O numero aleatorio3 foi: " + numeroAleatorio);
+Console.WriteLine("O numero aleatoriorio foi: " + numeroAleatorio);
+
+if(numeroDigitado == numeroAleatorio)
+{
+    Console.WriteLine("Parebens, voce acertou! O numero era" + numeroAleatorio);
+}
+
+else if (numeroDigitado > numeroAleatorio)
+{
+    Console.WriteLine("O numero digitado foi maior que o numero aleatorio!")
+}
+
+else
+{
+    Console.WriteLine("O numero digitado foi menor que o numero aleatorio!")
+}
 
 Console.ReadLine();
